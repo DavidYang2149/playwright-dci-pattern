419b61042bcb5c5ee0772bf1ac87289396d44b68
Davidyang2149 <davidyang2149@gmail.com>
2024-06-15 20:39:19 +0900

    ✨ #1 Feat: dci(Describe-Context-It) 패턴 구조를 생성하라

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export function describe(title: string, callback: () => void): void;
+
+export function context(title: string, callback: () => void): void;
+
+export function it(title: string, callback: () => void): void;
